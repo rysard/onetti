d83562b5c62d60ee216fb903ac9ea471c177fae9
Ricardo Colin <david_ricardo12@hotmail.com>
2017-12-09 17:47:14 -0600

    Initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h1>Project Onetti</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus">
+</style>
